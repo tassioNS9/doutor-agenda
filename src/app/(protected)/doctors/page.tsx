34a21038c100c8172adeaf0daa18b5baae96d325
Tassio Neves <tassions@hotmail.com>
2025-05-28 18:07:29 -0300

    feat: create page container components

---
+++ src/app/(protected)/doctors/page.tsx
@@ -0,0 +1,47 @@
+import { Plus } from "lucide-react";
+import { headers } from "next/headers";
+import { redirect } from "next/navigation";
+
+import { Button } from "@/components/ui/button";
+import {
+  PageActions,
+  PageContainer,
+  PageDescription,
+  PageHeader,
+  PageHeaderContent,
+  PageTitle,
+} from "@/components/ui/page-container";
+import { auth } from "@/lib/auth";
+
+//import AddDoctorButton from "./_components/add-doctor-button";
+//import DoctorCard from "./_components/doctor-card";
+
+const DoctorsPage = async () => {
+  const session = await auth.api.getSession({
+    headers: await headers(),
+  });
+  if (!session?.user) {
+    redirect("/authentication");
+  }
+  if (!session.user.clinic) {
+    redirect("/clinic-form");
+  }
+  return (
+    <PageContainer>
+      <PageHeader>
+        <PageHeaderContent>
+          <PageTitle>Médicos</PageTitle>
+          <PageDescription>Gerencie os médicos da sua clínica</PageDescription>
+        </PageHeaderContent>
+        <PageActions>
+          <Button>
+            <Plus />
+            Adicionar médico
+          </Button>
+        </PageActions>
+      </PageHeader>
+    </PageContainer>
+  );
+};
+
+export default DoctorsPage;
