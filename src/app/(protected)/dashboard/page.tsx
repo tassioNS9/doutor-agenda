63ac46f793bac5daf2a8ad1c36ad28d5b272baf0
Tassio Neves <tassions@hotmail.com>
2025-06-08 13:29:46 -0300

    feat: top specialties list

--- src/app/(protected)/dashboard/page.tsx
+++ src/app/(protected)/dashboard/page.tsx
@@ -22,7 +22,6 @@ import AppointmentsChart from "./_components/appointments-chart";
 import { DatePicker } from "./_components/date-picker";
 import StatsCards from "./_components/stats-cards";
 import TopDoctors from "./_components/top-doctors";
-import TopSpecialties from "./_components/top-specialties";
 
 interface DashboardPageProps {
   searchParams: Promise<{
@@ -41,9 +40,9 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
   if (!session.user.clinic) {
     redirect("/clinic-form");
   }
-  if (!session.user.plan) {
-    redirect("/new-subscription");
-  }
+  // if (!session.user.plan) {
+  //   redirect("/new-subscription");
+  // }
   const { from, to } = await searchParams;
   if (!from || !to) {
     redirect(
@@ -56,7 +55,6 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
     totalPatients,
     totalDoctors,
     topDoctors,
-    topSpecialties,
     todayAppointments,
     dailyAppointmentsData,
   } = await getDashboard({
@@ -112,7 +110,7 @@ const DashboardPage = async ({ searchParams }: DashboardPageProps) => {
               />
             </CardContent>
           </Card>
-          <TopSpecialties topSpecialties={topSpecialties} />
+          {/* <TopSpecialties topSpecialties={topSpecialties} /> */}
         </div>
       </PageContent>
     </PageContainer>
